f3f314688e95a6a7438bb3777a31439ba97a5e72
Ahme-cmyk <ahmedhosny1087@gmail.com>
2026-05-23 23:35:01 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,6 @@ def load_models():
     model2_path = hf_hub_download(repo_id="اسم_اليوزر_بتاعك/اسم_المستودع", filename="plant_model.keras")
     
     # تحميل الموديلات بعد تنزيلها محلياً في السيرفر
-    model1 = tf.keras.models.load_model(model1_path, compile=False)
-    model2 = tf.keras.models.load_model(model2_path, compile=False)
+   model1_path = hf_hub_download(repo_id="Ahme-cmyk/TRI-SCIENCE-AI", filename="health_model.keras")
+model2_path = hf_hub_download(repo_id="Ahme-cmyk/TRI-SCIENCE-AI", filename="plant_model.keras")
     return model1, model2
