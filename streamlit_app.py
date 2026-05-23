37cb5002883c3461c94198a9d3a12bb8ca78ce80
Ahme-cmyk <ahmedhosny1087@gmail.com>
2026-05-23 23:37:35 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,17 @@
-from huggingface_hub import hf_hub_download
+import streamlit as st
 import tensorflow as tf
+from huggingface_hub import hf_hub_download
 
 @st.cache_resource
 def load_models():
-    # تحميل الملفات من Hugging Face
-    model1_path = hf_hub_download(repo_id="اسم_اليوزر_بتاعك/اسم_المستودع", filename="health_model.keras")
-    model2_path = hf_hub_download(repo_id="اسم_اليوزر_بتاعك/اسم_المستودع", filename="plant_model.keras")
+    # تحميل الموديلات من مستودع Hugging Face الخاص بك
+    model1_path = hf_hub_download(repo_id="Ahme-cmyk/TRI-SCIENCE-AI", filename="health_model.keras")
+    model2_path = hf_hub_download(repo_id="Ahme-cmyk/TRI-SCIENCE-AI", filename="plant_model.keras")
     
-    # تحميل الموديلات بعد تنزيلها محلياً في السيرفر
-   model1_path = hf_hub_download(repo_id="Ahme-cmyk/TRI-SCIENCE-AI", filename="health_model.keras")
-model2_path = hf_hub_download(repo_id="Ahme-cmyk/TRI-SCIENCE-AI", filename="plant_model.keras")
+    # تحميل الموديلات بعد تنزيلها
+    model1 = tf.keras.models.load_model(model1_path, compile=False)
+    model2 = tf.keras.models.load_model(model2_path, compile=False)
     return model1, model2
+
+# استدعاء الدالة
+model1, model2 = load_models()
