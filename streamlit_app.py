6aa90a65260afca0108f82e8a8f6febb17a422a5
Ahme-cmyk <ahmedhosny1087@gmail.com>
2026-05-23 23:15:48 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,12 @@
 import streamlit as st
 import tensorflow as tf
-import requests
-import os
 
-# نستخدم هذا السطر للتأكد من استخدام النسخة المدمجة مع تنسرفلو
-from tensorflow import keras
-
+# تحميل الموديل من نفس المجلد
 @st.cache_resource
 def load_models():
-    # ... (نفس الكود السابق لتحميل الملفات من الرابط) ...
-    
-    # التعديل هنا: استخدام tf.keras.models.load_model مباشرة
+    # طالما الملفات مرفوعة في نفس فولدر الكود، المسار بيكون اسم الملف بس
     model1 = tf.keras.models.load_model("health_model.keras", compile=False)
     model2 = tf.keras.models.load_model("plant_model.keras", compile=False)
     return model1, model2
+
+model1, model2 = load_models()
